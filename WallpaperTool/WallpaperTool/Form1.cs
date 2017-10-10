e19ed6b203da4447e1582a425822477d8254989c
zhsir123 <zhsir123@gmail.com>
2017-10-10 23:49:44 +0900

    이미지 출력(바탕화면에 안맞춰짐)

--- WallpaperTool/WallpaperTool/Form1.cs
+++ WallpaperTool/WallpaperTool/Form1.cs
@@ -20,19 +20,30 @@ namespace WallpaperTool
             this.FormBorderStyle = FormBorderStyle.None;
             this.Location = new Point(0, 0);
             this.Size = Screen.PrimaryScreen.Bounds.Size;
-            
 
+            DrawImage();
         }
 
-        protected override void OnPaint(PaintEventArgs e)
+        // rainbow background
+        //protected override void OnPaint(PaintEventArgs e)
+        //{
+        //    LinearGradientBrush brush = new LinearGradientBrush(this.ClientRectangle, Color.Black, Color.Black, 0, false);
+        //    ColorBlend cb = new ColorBlend();
+        //    cb.Positions = new[] { 0, 1 / 6f, 2 / 6f, 3 / 6f, 4 / 6f, 5 / 6f, 1 };
+        //    cb.Colors = new[] { Color.Red, Color.Orange, Color.Yellow, Color.Green, Color.Blue, Color.Navy, Color.Purple };
+        //    brush.InterpolationColors = cb;
+        //    e.Graphics.FillRectangle(brush, this.ClientRectangle);
+        //}
+
+        private void DrawImage()
         {
-            LinearGradientBrush brush = new LinearGradientBrush(this.ClientRectangle, Color.Black, Color.Black, 0, false);
-            ColorBlend cb = new ColorBlend();
-            cb.Positions = new[] { 0, 1 / 6f, 2 / 6f, 3 / 6f, 4 / 6f, 5 / 6f, 1 };
-            cb.Colors = new[] { Color.Red, Color.Orange, Color.Yellow, Color.Green, Color.Blue, Color.Navy, Color.Purple };
-            brush.InterpolationColors = cb;
-            e.Graphics.FillRectangle(brush, this.ClientRectangle);
+            pictureBox1.Image = System.Drawing.Image.FromFile(@"C:\Users\dsm2016\Pictures\600.jpg");
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
         }
-        
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
     }
 }
